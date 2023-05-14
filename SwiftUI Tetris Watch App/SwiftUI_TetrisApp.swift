0c3fbb946e231f9735ec923a7468e6e847b134d8
Sameer Sundresh <sundresh@sundresh.org>
2023-05-14 15:52:33 -0600

    Initial Commit

---
+++ SwiftUI Tetris Watch App/SwiftUI_TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TetrisApp.swift
+//  SwiftUI Tetris Watch App
+//
+//  Created by Sameer Sundresh on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tetris_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
